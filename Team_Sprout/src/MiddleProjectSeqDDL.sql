e3a223779950b49a88c09c812c701cf8c187117a
Straingers <78468109+Straingers@users.noreply.github.com>
2021-07-28 11:35:11 +0900

    Add files via upload

---
+++ Team_Sprout/src/MiddleProjectSeqDDL.sql
@@ -0,0 +1,93 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 수요일-7월-28-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence ATCH_FILE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."ATCH_FILE_SEQ"  MINVALUE 1 MAXVALUE 99999999999 INCREMENT BY 1 START WITH 921 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence COMMUNITY_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."COMMUNITY_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence COMMUNITY_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."COMMUNITY_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 281 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence FAQ_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."FAQ_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence FAQ_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."FAQ_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 301 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence FREE_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."FREE_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 241 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence FREE_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."FREE_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 481 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence NOTICE_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."NOTICE_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 261 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence NOTICE_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."NOTICE_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 561 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence PROJECT_OPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."PROJECT_OPTION_SEQ"  MINVALUE 1 MAXVALUE 999999 INCREMENT BY 1 START WITH 241 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence PROJECT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."PROJECT_SEQ"  MINVALUE 500 MAXVALUE 99999999 INCREMENT BY 1 START WITH 740 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence QNA_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."QNA_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 241 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence QNA_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."QNA_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 401 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence REPORT_BOARD_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."REPORT_BOARD_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence REPORT_BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."REPORT_BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 301 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TB_ATCH_FILE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."TB_ATCH_FILE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TB_ORDER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."TB_ORDER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 341 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TB_PROJECT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TEAM2_202101M"."TB_PROJECT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
